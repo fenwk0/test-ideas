4f6fc4c8de92e90b328c2fbeb42c2397f6ac5e23
fenwk0 <karl.fenwick@gmail.com>
2018-11-21 21:16:54 +0000

    Initial stuff

--- format/strings.py
+++ format/strings.py
@@ -1,6 +1,24 @@
-def create_command():
-    command_template = "cmd {parma1} {param2} {param3}"
+def create_command_001():
+    command_template = "cmd_001 {param1} {param2} {param3}"
     return command_template.format(param1="one", param2="two", param3="three")
 
 
-print(create_command())
+def create_command_002():
+    command_template = "cmd_002 "\
+                       + "{param1} "\
+                       + "{param2} "\
+                       + "{param3}"
+    return command_template.format(param1="one", param2="two", param3="three")
+
+
+def create_command_003():
+    command_template = ("cmd_003 "
+                        + "{param1} "
+                        + "{param2} "
+                        + "{param3}")
+    return command_template.format(param1="one", param2="two", param3="three")
+
+
+print(create_command_001())
+print(create_command_002())
+print(create_command_003())
